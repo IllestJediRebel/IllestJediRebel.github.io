df0f76d0adb9b16b220655b1a4c09a0197edb80d
Reese Myricks <phillybred2627@gmail.com>
2016-06-20 17:56:58 +0000

    index file

--- dice/main.js
+++ dice/main.js
@@ -1,1 +1,14 @@
-<center><h2>Dice App!</h2></center>
+<!doctype html>
+<head>
+<meta charset="utf-8">
+<title>Dice App</title>
+<link type="styesheet" href="style.css">
+<link type="icon" href=" ../img/die5.png">
+</head>
+<body>
+<div class="appHolder">
+<h4>Dice App</h4>
+<div id="numberHolder">10</div>
+</div>
+</body>
+
